eac51b17cdd884f7f5bc1c6220114a97c3e6ec3c
Guilherme de Oliveira Vicente <guilhermedovicente@gmail.com>
2023-06-03 10:47:24 -0300

    gitflow-feature-stash: atividade-08
    Adicionando o materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'petvac';
+
+  ngAfterViewInit(): void {
+      //M.Sidenav.init(this.sideNav?.nativeElement);
+  }
 }
